44e6dce584a8c465f41cfc63270f7130b60f950f
ayskbdyi <ayskbdyi@gmail.com>
2019-07-29 09:36:38 +0300

    Giriş Sayfaları ve HomeFragment(Son)

--- app/src/main/java/com/turkcell/sence/activities/LoginActivity.java
+++ app/src/main/java/com/turkcell/sence/activities/LoginActivity.java
@@ -4,11 +4,10 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
+
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
