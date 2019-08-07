108739d8c1cc174493ea3f5737118dcb8c803a14
ayskbdyi <ayskbdyi@gmail.com>
2019-08-07 23:48:23 +0300

    Revert "takip ettiklerim ve takipçiler listview ve adaptörler oluşturuldu"
    This reverts commit 7fe523d

--- app/src/main/java/com/turkcell/sence/fragments/UserProfileFollowingFragment.java
+++ app/src/main/java/com/turkcell/sence/fragments/UserProfileFollowingFragment.java
@@ -5,16 +5,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import com.turkcell.sence.R;
-import com.turkcell.sence.adapters.FollowingListCustomAdapter;
-import com.turkcell.sence.adapters.UserProfileSurveyCustomAdapter;
-import com.turkcell.sence.models.User;
-import com.turkcell.sence.models.UserProfileSurvey;
-
-import java.util.ArrayList;
-import java.util.Calendar;
 
 
 public class UserProfileFollowingFragment extends Fragment {
@@ -22,13 +14,6 @@ public class UserProfileFollowingFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    ListView userListView;
-    ArrayList<User> userList = new ArrayList<>();
-    FollowingListCustomAdapter adapter;
-
-
-
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -69,18 +54,8 @@ public class UserProfileFollowingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_user_profile_following, container, false);
-
-        userListView = view.findViewById(R.id.followingList_lv);
-        userList.add(new User("ds","dido","didem öz","fds","fsdf",R.drawable.ic_home_black_24dp));
-        userList.add(new User("ds","dido","didem öz d","fds","fsdf",R.drawable.ic_home_black_24dp));
-        userList.add(new User("ds","dido","didem öz t","fds","fsdf",R.drawable.ic_home_black_24dp));
-        userList.add(new User("ds","dido","didem öz rf","fds","fsdf",R.drawable.ic_home_black_24dp));
-        adapter = new FollowingListCustomAdapter(getContext(),userList);
-        userListView.setAdapter(adapter);
         // Inflate the layout for this fragment
-        return view;
+        return inflater.inflate(R.layout.fragment_user_profile_following, container, false);
     }
 
 
